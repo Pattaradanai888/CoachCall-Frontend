63eed73c002fc003d1e4535949cd436d1eb8a763
Pattaradanai888 <pattaradanai_t@cmu.ac.th>
2025-06-16 19:09:48 +0700

    Hot-Fix; Apply proper handler for access_token and refresh_token expire.
    This code is adding check the return api status if it 401 then send the /refresh request to get new access_token but if the /refresh return 401 then logout

--- app.vue
+++ app.vue
@@ -5,3 +5,26 @@
     </NuxtLayout>
   </div>
 </template>
+
+<script setup lang="ts">
+import { navigateTo, useRoute } from '#imports';
+import { nextTick, onMounted, watch } from 'vue';
+import { useAuthStore } from '~/stores/auth';
+
+onMounted(() => {
+  const authStore = useAuthStore();
+  const route = useRoute();
+
+  watch(
+    () => authStore.isAuthenticated,
+    async (isAuthenticated, wasAuthenticated) => {
+      if (wasAuthenticated && !isAuthenticated) {
+        if (route.path !== '/login' && route.path !== '/register') {
+          await nextTick();
+          navigateTo('/login', { replace: true });
+        }
+      }
+    },
+  );
+});
+</script>
